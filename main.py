8296f98c2739cee27725c2d223e3e143e1ded33c
Mateusz Kraiński <mt.krainski@gmail.com>
2018-11-19 11:37:19 +0100

    Using RGB instead of TCI

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import rasterio
-import geopyspark as gps
+# import geopyspark as gps
 import numpy as np
 import zipfile
 import warnings
 import os
+import matplotlib.pyplot as plt
 
 from pyspark import SparkContext
 
@@ -12,10 +13,10 @@ class ArchiveContentsWarning(ResourceWarning):
 
 print("Starting script...")
 
-conf = gps.geopyspark_conf(
-    master="local[*]",
-    appName="sentinel-ingest-example",)
-pysc = SparkContext(conf=conf)
+# conf = gps.geopyspark_conf(
+#         master="local[*]",
+#         appName="sentinel-ingest-example")
+# pysc = SparkContext(conf=conf)
 
 DATA_PATH = "data"
 SAT_TYPE = "S2A"
@@ -26,25 +27,30 @@ RELATIVE_ORBIT_NUMBER = "R022"
 TILE_NUMBER_FIELD = "T33UVR"
 TIMESTAMP = "20181016T121930"
 DATASET_NAME = "_".join([
-    SAT_TYPE,
-    PROD_LEVEL,
-    SENSING_START,
-    PROC_BASELINE,
-    RELATIVE_ORBIT_NUMBER,
-    TILE_NUMBER_FIELD,
-    TIMESTAMP,
+        SAT_TYPE,
+        PROD_LEVEL,
+        SENSING_START,
+        PROC_BASELINE,
+        RELATIVE_ORBIT_NUMBER,
+        TILE_NUMBER_FIELD,
+        TIMESTAMP,
 ])
 #"S2A_MSIL1C_20181016T101021_N0206_R022_T33UVR_20181016T121930"
 ARCHIVE_EXT = ".zip"
 
 TCI_FILE_KEYWORD = "TCI.jp2"
+RED_CHANNEL_KEYWORD = "B04.jp2"
+GREEN_CHANNEL_KEYWORD = "B03.jp2"
+BLUE_CHANNEL_KEYWORD = "B02.jp2"
 
 IMAGE_PATH = ""
 
-def get_tci_image_filename(archive):
+ARCHIVE_PATH = os.path.join(DATA_PATH, DATASET_NAME + ARCHIVE_EXT)
+
+def get_image_filename(archive, keyword=TCI_FILE_KEYWORD):
     image_file = None
     for file in archive.infolist():
-        if TCI_FILE_KEYWORD in file.filename:
+        if keyword in file.filename:
             if image_file is None:
                 image_file = file.filename
             else:
@@ -53,29 +59,38 @@ def get_tci_image_filename(archive):
 
 print("Extracting image path...")
 
-archive_path = os.path.join(DATA_PATH, DATASET_NAME+ARCHIVE_EXT)
+archive = zipfile.ZipFile(ARCHIVE_PATH)
 
-archive = zipfile.ZipFile(archive_path)
-
-image_file = get_tci_image_filename(archive)
+tci_image_filename = get_image_filename(archive)
+red_image_filename = get_image_filename(archive, RED_CHANNEL_KEYWORD)
+green_image_filename = get_image_filename(archive, GREEN_CHANNEL_KEYWORD)
+blue_image_filename = get_image_filename(archive, BLUE_CHANNEL_KEYWORD)
 
 print("Loading image...")
 
-# Based on https://geopyspark.readthedocs.io/en/latest/tutorials/reading-in-sentinel-data.html
-with rasterio.open("zip://" + archive_path + "!" + image_file) as f:
-    image = f.read(1)
-    extent = gps.Extent(*f.bounds)
-    projected_extent = gps.ProjectedExtent(
-        extent=extent,
-        epsg=int(f.crs.to_dict()['init'][5:]))
-    tile = gps.Tile.from_numpy_array(
-        numpy_array=image,
-        no_data_value=f.nodata)
-    rdd = pysc.parallelize([(projected_extent, tile)])
-    raster_layer = gps.RasterLayer.from_numpy_rdd(
-        layer_type=gps.LayerType.SPATIAL,
-        numpy_rdd=rdd)
+def read_archive_file(image_filename, archive_path = ARCHIVE_PATH):
+    # Based on https://geopyspark.readthedocs.io/en/latest/tutorials/reading-in-sentinel-data.html
+    with rasterio.open("zip://" + archive_path + "!" + image_filename) as f:
+        image = f.read(1)
+    return image
 
+tci_image = read_archive_file(tci_image_filename)
+red_image = read_archive_file(red_image_filename)
+green_image = read_archive_file(green_image_filename)
+blue_image = read_archive_file(blue_image_filename)
+
+print("Composing RGB image...")
+
+color_image = np.stack(
+        (red_image, green_image, blue_image),
+        axis=2)
+
+color_image = color_image/max(color_image)
+
+print("Plotting...")
+imgplot = plt.imshow(color_image)
+# imgplot.set_cmap('gnuplot')
+plt.show()
 
 def read_zip_file(filepath):
     zfile = zipfile.ZipFile(filepath)
