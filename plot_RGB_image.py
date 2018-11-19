5e4a684dd8d87f5f5c47675917beb73b78c98d55
Mateusz Kraiński <mt.krainski@gmail.com>
2018-11-19 13:55:49 +0100

    refactored to a set of functions

--- plot_RGB_image.py
+++ plot_RGB_image.py
@@ -35,7 +35,7 @@ RED_CHANNEL_KEYWORD = "B04.jp2"
 GREEN_CHANNEL_KEYWORD = "B03.jp2"
 BLUE_CHANNEL_KEYWORD = "B02.jp2"
 
-VALUE_THRESHOLD = 3000
+VALUE_THRESHOLD = 2000
 
 IMAGE_PATH = ""
 
@@ -51,59 +51,59 @@ def get_image_filename(archive, keyword=TCI_FILE_KEYWORD):
                 warnings.warn("Warning! More than one TCI file found!", ArchiveContentsWarning)
     return image_file
 
-print("Extracting image path...")
-
-archive = zipfile.ZipFile(ARCHIVE_PATH)
-
-tci_image_filename = get_image_filename(archive)
-red_image_filename = get_image_filename(archive, RED_CHANNEL_KEYWORD)
-green_image_filename = get_image_filename(archive, GREEN_CHANNEL_KEYWORD)
-blue_image_filename = get_image_filename(archive, BLUE_CHANNEL_KEYWORD)
-
-print("Loading image...")
-
 def read_archive_file(image_filename, archive_path = ARCHIVE_PATH):
     # Based on https://geopyspark.readthedocs.io/en/latest/tutorials/reading-in-sentinel-data.html
     with rasterio.open("zip://" + archive_path + "!" + image_filename) as f:
         image = f.read(1)
     return image
 
-red_image = read_archive_file(red_image_filename)
-print("\tRED read")
-green_image = read_archive_file(green_image_filename)
-print("\tGREEN read")
-blue_image = read_archive_file(blue_image_filename)
-print("\tBLUE read")
 
-print("Composing RGB image...")
+def get_rgb_from_archive(archive_path = ARCHIVE_PATH):
 
-color_image = np.stack(
-        (red_image, green_image, blue_image),
-        axis=2)
+    print("Extracting image path...")
 
-del red_image, green_image, blue_image
+    archive = zipfile.ZipFile(archive_path)
 
-np.place(
-    color_image,
-    color_image>VALUE_THRESHOLD,
-    VALUE_THRESHOLD)
+    tci_image_filename = get_image_filename(archive)
+    red_image_filename = get_image_filename(archive, RED_CHANNEL_KEYWORD)
+    green_image_filename = get_image_filename(archive, GREEN_CHANNEL_KEYWORD)
+    blue_image_filename = get_image_filename(archive, BLUE_CHANNEL_KEYWORD)
 
-norm_factor = np.max(color_image) / 255
+    print("Loading image...")
 
-# Processing row by row to save memory on conversion from int
-# to float due to division
-color_image = np.array(
-    [(row/norm_factor).astype(np.uint8) for row in color_image]
-)
+    red_image = read_archive_file(red_image_filename)
+    print("\tRED read")
+    green_image = read_archive_file(green_image_filename)
+    print("\tGREEN read")
+    blue_image = read_archive_file(blue_image_filename)
+    print("\tBLUE read")
+
+    print("Composing RGB image...")
+
+    color_image = np.stack(
+            (red_image, green_image, blue_image),
+            axis=2)
+
+    del red_image, green_image, blue_image
+
+    np.place(
+        color_image,
+        color_image>VALUE_THRESHOLD,
+        VALUE_THRESHOLD)
+
+    norm_factor = np.max(color_image) / 255
+
+    # Processing row by row to save memory on conversion from int
+    # to float due to division
+    color_image = np.array(
+        [(row/norm_factor).astype(np.uint8) for row in color_image]
+    )
+
+    return color_image
+
+image = get_rgb_from_archive()
 
 print("Plotting...")
-imgplot = plt.imshow(color_image)
+imgplot = plt.imshow(image)
 plt.show()
-
-def read_zip_file(filepath):
-    zfile = zipfile.ZipFile(filepath)
-    for finfo in zfile.infolist():
-        ifile = zfile.open(finfo)
-        line_list = ifile.readlines()
-        print(line_list)
 
