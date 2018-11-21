3c6d3430dc0906f671f56ee99ad7051b90cc4667
Mateusz Kraiński <mt.krainski@gmail.com>
2018-11-21 17:09:10 +0100

    renaming files

--- download_and_plot.py
+++ download_and_plot.py
@@ -1,5 +1,5 @@
-import request_last_dataset
-import plot_RGB_image
+import sentinel_requests
+import images
 import os
 
 SAMPLE_LOCATION = (50.083333, 14.416667) # Prague
@@ -9,19 +9,19 @@ IMAGE_PATH = "saved_images"
 ARCHIVE_EXT = ".zip"
 
 print("Forming request...")
-response = request_last_dataset.request_sentinel_2_data(SAMPLE_LOCATION)
+response = sentinel_requests.request_sentinel_2_data(SAMPLE_LOCATION)
 
 if response.status_code == 200:
     print("Request successful.")
 
 entry = response.json()['feed']['entry'][0]
 
-dataset_name = request_last_dataset.download(entry)
+dataset_name = sentinel_requests.download(entry)
 
 archive_path = os.path.join(DATA_PATH,
                             dataset_name+ARCHIVE_EXT)
 
-image = plot_RGB_image.get_tci_image(archive_path)
+image = images.get_tci_image(archive_path)
 
 print("Plotting...")
-plot_RGB_image.plot_and_save(image, dataset_name)
+images.plot_and_save(image, dataset_name)
