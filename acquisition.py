da28e47ed3e94c9482022bbef04b5790784ed6cc
Mateusz Kraiński <mt.krainski@gmail.com>
2018-11-25 21:58:15 +0100

    Tested acquisition and added script functionality

--- acquisition.py
+++ acquisition.py
@@ -1,9 +1,32 @@
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="Sentinel 2 acquisition checker",
+        description="This script allows you to quickly check "
+                    "when either one of the Sentinel 2 satellites "
+                    "will be taking pictures on a given location. "
+                    "Information is extracted directly from "
+                    "Sentinel 2 acquisition plans, available at "
+                    "https://sentinel.esa.int "
+                    "Sample location: 50.083333 14.416667 (Prague)",
+        usage=f"python {__file__} --location lat lon",
+    )
+
+    parser.add_argument('--location', '-l', nargs=2, type=float, required=True)
+
+    args = parser.parse_args()
+
+
 from bs4 import BeautifulSoup
 import requests
 from lxml import etree
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-import datetime
+from datetime import datetime
+
+DEBUG = False
 
 class AcquisitionSwath:
     def __init__(self,
@@ -43,15 +66,19 @@ class AcquisitionSwath:
         class_object = AcquisitionSwath()
         class_object.name = placemark_xml.xpath(AcquisitionSwath.NAME_PATH, namespaces=nsmap)[0].text
 
-        for data_object in placemark_xml.find(AcquisitionSwath.DATA_PATH).iter():
-            class_object.__dict__[data_object.get('name')] = data_object.find('kml:value', namespaces=nsmap).text
+        for data_object in placemark_xml.find(AcquisitionSwath.DATA_PATH, namespaces=nsmap).iter("{http://www.opengis.net/kml/2.2}Data"):
+            value_item = data_object.find('kml:value', namespaces=nsmap)
+            if value_item is not None:
+                class_object.__dict__[data_object.get('name')] = value_item.text
+            elif DEBUG:
+                print(f"Tag {data_object.get('name')}, from {class_object.name} didn't have value!")
 
         class_object.parse_dates()
         polygon_coordinates = placemark_xml.find(AcquisitionSwath.POLYGON_PATH,
                                                  namespaces=nsmap).text
         polygon_definition = []
 
-        for coordinate in polygon_coordinates.split(" "):
+        for coordinate in polygon_coordinates.strip().split(" "):
             point = [float(x) for x in coordinate.split(',')]
             polygon_definition.append(point[0:2])
 
@@ -60,8 +87,8 @@ class AcquisitionSwath:
         return class_object
 
     def parse_dates(self):
-        self.ObservationTimeStart = datetime.datetime.strptime(self.ObservationTimeStart, "%Y-%m-%dT%H:%M:%S.%fZ")
-        self.ObservationTimeStop = datetime.datetime.strptime(self.ObservationTimeStop, "%Y-%m-%dT%H:%M:%S.%fZ")
+        self.ObservationTimeStart = datetime.strptime(self.ObservationTimeStart, "%Y-%m-%dT%H:%M:%S.%f")
+        self.ObservationTimeStop = datetime.strptime(self.ObservationTimeStop, "%Y-%m-%dT%H:%M:%S.%f")
 
     def get_acquisition_period(self):
         return [self.ObservationTimeStart, self.ObservationTimeStop]
@@ -73,7 +100,7 @@ class AcquisitionSwath:
         return self.polygon.contains(point)
 
 
-def get_acquisition_plan(kml_file):
+def decode_kml_file(kml_file):
     PLACEMARK_PATH = "kml:Placemark"
     nsmap = kml_file.nsmap
     if None in nsmap:
@@ -85,28 +112,48 @@ def get_acquisition_plan(kml_file):
     return placemarks_decoded
 
 
+def get_acquisition_plan():
 
-SAMPLE_LOCATION = (50.083333, 14.416667) # Prague
+    SENTINEL_URL_BASE = "https://sentinel.esa.int"
+    ACQUISITION_PLANS = "/web/sentinel/missions/sentinel-2/acquisition-plans"
 
-SENTINEL_URL_BASE = "https://sentinel.esa.int"
-ACQUISITION_PLANS = "/web/sentinel/missions/sentinel-2/acquisition-plans"
+    acquisition_response = requests.get(f"{SENTINEL_URL_BASE}{ACQUISITION_PLANS}").text
 
-acquisition_html = requests.get(f"{SENTINEL_URL_BASE}{ACQUISITION_PLANS}").text
+    page_parser = BeautifulSoup(acquisition_response, 'html.parser')
+    plan_link_a = page_parser.find('div', 'sentinel-2a').find('a')['href']
+    plan_link_b = page_parser.find('div', 'sentinel-2b').find('a')['href']
 
-page_parser = BeautifulSoup(acquisition_html, 'html.parser')
-plan_link_a = page_parser.find('div', 'sentinel-2a').find('a')['href']
-plan_link_b = page_parser.find('div', 'sentinel-2b').find('a')['href']
+    kml_a = etree.XML(requests.get(f"{SENTINEL_URL_BASE}{plan_link_a}").content)
+    kml_b = etree.XML(requests.get(f"{SENTINEL_URL_BASE}{plan_link_b}").content)
 
-kml_a = etree.XML(requests.get(f"{SENTINEL_URL_BASE}{plan_link_a}").content)
-kml_b = etree.XML(requests.get(f"{SENTINEL_URL_BASE}{plan_link_b}").content)
+    return decode_kml_file(kml_a) + decode_kml_file(kml_b)
 
-placemarks = get_acquisition_plan(kml_a) + get_acquisition_plan(kml_b)
 
-matching_acquisitions = []
+if __name__ == "__main__":
 
-for placemark in placemarks:
-    if placemark.is_point_in_polygon(SAMPLE_LOCATION):
-        matching_acquisitions.append(placemark.get_acquisition_period())
+    location = args.location
 
-print("Matching acquisitions:")
-print(matching_acquisitions)
+    # SAMPLE_LOCATION = (50.083333, 14.416667) # Prague
+
+    placemarks = get_acquisition_plan()
+
+    matching_acquisitions = []
+
+    for placemark in placemarks:
+        if placemark.is_point_in_polygon(location):
+            matching_acquisitions.append(placemark.get_acquisition_period())
+
+    print("Matching acquisitions (dd.mm.yyyy 24h time, UTC):")
+    for acq in matching_acquisitions:
+        decode_format_full = "%d.%m.%Y %H:%M:%S"
+        decode_format_time = "%H:%M:%S"
+        decode_format_date = "%d.%m.%Y"
+
+        if acq[0].date() == acq[1].date():
+            date = acq[0].strftime(decode_format_date)
+            time_start = acq[0].strftime(decode_format_time)
+            time_end = acq[1].strftime(decode_format_time)
+            print(f"{date} {time_start} - {time_end}")
+        else:
+            print(f"{acq[0].strftime(decode_format_full)} "
+                  f"- {acq[1].strftime(decode_format_full)}")
