d544db96ac114e449d5b1fbd3383cfe2f5dbf4ea
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-18 17:19:20 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='sentinel2',
       version='0.1',
       description='simple library for handling sentinel 2 requests and images',
@@ -7,5 +10,6 @@ setup(name='sentinel2',
       author='M. Krainski',
       author_email='mateusz@krainski.eu',
       license='MIT',
+      install_requires=required,
       packages=['sentinel2'],
       zip_safe=False)
